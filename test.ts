cddb3f67f82b495e45882f95b3f30457c7906a45
shaoziyang <shaoziyang@126.com>
2019-06-07 12:36:30 +0800

    modify test

--- test.ts
+++ test.ts
@@ -1,12 +1,6 @@
-let temperature = 0
-let humidity = 0
-let pressure = 0
-basic.forever(() => {
-    pressure = BME280.pressure()
-    humidity = BME280.humidity()
-    temperature = BME280.temperature()
-    serial.writeValue("P", pressure)
-    serial.writeValue("H", humidity)
-    serial.writeValue("T", temperature)
+basic.forever(function () {
+    serial.writeValue("T", SW01.temperature())
+    serial.writeValue("H", SW01.temperature())
+    serial.writeValue("P", SW01.pressure())
     basic.pause(1000)
 })
