872b0cd17be7c31540c3999cf0d879d2d0b4795d
shaoziyang <shaoziyang@126.com>
2019-06-25 22:05:20 +0800

    add Events

--- sw01.ts
+++ sw01.ts
@@ -175,6 +175,101 @@ namespace SW01 {
         return T - Math.idiv(100 - H, 5)
     }
 
+    /**
+     * Pressure below Event
+     */
+    //% block="Pressure below than %dat" dat.defl=100000
+    export function PressureBelowThan(dat: number, body: () => void): void {
+        control.inBackground(function () {
+            while (true) {
+                get()
+                if (P < dat) {
+                    body()
+                }
+                basic.pause(1000)
+            }
+        })
+    }
+
+    /**
+     * Pressure higher Event
+     */
+    //% block="Pressure higher than %dat" dat.defl=100000
+    export function PressureHigherThan(dat: number, body: () => void): void {
+        control.inBackground(function () {
+            while (true) {
+                get()
+                if (P > dat) {
+                    body()
+                }
+                basic.pause(1000)
+            }
+        })
+    }
+
+    /**
+     * humidity below Event
+     */
+    //% block="Humidity below than %dat" dat.defl=10
+    export function HumidityBelowThan(dat: number, body: () => void): void {
+        control.inBackground(function () {
+            while (true) {
+                get()
+                if (H < dat) {
+                    body()
+                }
+                basic.pause(1000)
+            }
+        })
+    }
+
+    /**
+     * humidity higher Event
+     */
+    //% block="Humidity higher than %dat" dat.defl=50
+    export function HumidityHigherThan(dat: number, body: () => void): void {
+        control.inBackground(function () {
+            while (true) {
+                get()
+                if (H > dat) {
+                    body()
+                }
+                basic.pause(1000)
+            }
+        })
+    }
+
+    /**
+     * temperature below Event
+     */
+    //% block="Temperature below than %dat" dat.defl=10
+    export function TemperatureBelowThan(dat: number, body: () => void): void {
+        control.inBackground(function () {
+            while (true) {
+                get()
+                if (T < dat) {
+                    body()
+                }
+                basic.pause(1000)
+            }
+        })
+    }
+
+    /**
+     * temperature higher Event
+     */
+    //% block="Temperature higher than %dat" dat.defl=30
+    export function TemperatureHigherThan(dat: number, body: () => void): void {
+        control.inBackground(function () {
+            while (true) {
+                get()
+                if (T > dat) {
+                    body()
+                }
+                basic.pause(1000)
+            }
+        })
+    }
 
     /**
      * set I2C address
